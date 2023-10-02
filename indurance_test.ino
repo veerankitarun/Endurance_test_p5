4707db85d7f9649e8f151abb98610ccc5646effa
Veeranki Tarun Kumar Goud <61703896+veerankitarun@users.noreply.github.com>
2023-10-02 21:40:10 +0530

    Update indurance_test.ino

--- indurance_test.ino
+++ indurance_test.ino
@@ -1,15 +1,20 @@
 #include <Arduino.h>
 
-const int relayPin = 13; 
-int cycleCounter = 0;   
+const int relayPin = 13;
+int cycleCounter = 0;
+
+unsigned long startTime; // Variable to store the start time
 
 void setup() {
   // Initialize 
   pinMode(relayPin, OUTPUT);
 
-  // com Serial Monitor
+  // Start Serial Monitor
   Serial.begin(9600);
   Serial.println("Relay Control Started");
+
+  // Record the start time
+  startTime = millis();
 }
 
 void loop() {
@@ -31,22 +36,24 @@ void loop() {
     Serial.println(": Relay ON");
     delay(cycleTime / 2);
 
-  
     digitalWrite(relayPin, LOW);
     Serial.print("Cycle ");
     Serial.print(cycleCounter);
     Serial.println(": Relay OFF");
-    delay(cycleTime / 2); 
+    delay(cycleTime / 2);
 
-  
     Serial.print("Cycle ");
     Serial.print(cycleCounter);
-    Serial.println(": CycleCompleted");
+    Serial.println(": Cycle Completed");
   } else {
-    
+    unsigned long endTime = millis(); // Record the end time
     Serial.println("450 cycles completed. Stopping.");
+    Serial.print("Start Time: ");
+    Serial.println(startTime);
+    Serial.print("End Time: ");
+    Serial.println(endTime);
     while (true) {
-      
+      // Keep the program running
     }
   }
 }
